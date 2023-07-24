eae5c561700b987fbfb09cb157033718c5146520
Raihan-28011 <raihanrifar4@gmail.com>
2023-07-25 01:55:24 +0600

    added database configuration, working signup and login page, added labeval.sql file

---
+++ client/src/components/Select.js
@@ -0,0 +1,52 @@
+import { useEffect, useRef, useState } from "react";
+
+export default function Select({ options, setSelected }) {
+  const optionRef = useRef(null);
+  const [show, setShow] = useState(false);
+  const [currentOption, setCurrentOption] = useState(0);
+
+  useEffect(() => {
+    const handleMouseDown = (e) => {
+      if (optionRef?.current && !optionRef.current.contains(e.target))
+        setShow(false);
+    };
+    document.addEventListener("mousedown", handleMouseDown);
+
+    return () => document.removeEventListener("mousedown", handleMouseDown);
+  }, [optionRef]);
+
+  return (
+    <div
+      ref={optionRef}
+      className={
+        "select relative h-10 w-full flex flex-col justify-center px-4 transition-all duration-350 border border-solid border-slate-300 rounded-[5px] before:border-[3px] before:border-solid before:border-transparent before:border-t-slate-900 before:border-r-slate-900 before:rounded-[2px] before:absolute before:right-4 before:top-[12px] before:w-[10px] before:h-[10px] cursor-pointer" +
+        (show
+          ? " before:rotate-[-45deg] before:top-[15px]"
+          : " before:rotate-[135deg]")
+      }
+      onClick={() => setShow((prev) => !prev)}
+    >
+      <span className="transition-all duration-350">
+        {options[currentOption]}
+      </span>
+      {show ? (
+        <div className="select-options animate-opacity bg-[rgba(248,250,252,0.95)] backdrop-blur-[10px] w-[inherit] shadow-[0_0_8px_rgba(0,0,0,0.2)] z-10 rounded-[5px] flex flex-col absolute top-[105%] left-[0%]">
+          {options.map((value, index) => {
+            return (
+              <div
+                className="option flex flex-row h-10 px-4 items-center hover:bg-slate-200 cursor-pointer transition-all duration-300"
+                key={index}
+                onClick={() => {
+                  setCurrentOption(index);
+                  setSelected(index);
+                }}
+              >
+                {value}
+              </div>
+            );
+          })}
+        </div>
+      ) : null}
+    </div>
+  );
+}
--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -1,9 +1,64 @@
+import axios from "axios";
+import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/router";
+import { useState } from "react";
+
+import Select from "@/components/Select";
 
 export default function Login() {
+  const [username, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [selected, setSelected] = useState(0);
+  const [loading, setLoading] = useState(false);
+  const router = useRouter();
+
+  const handleError = (s) => {
+    setError(s);
+    setTimeout(() => {
+      setError("");
+    }, 5000);
+  };
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    axios
+      .post("/api/login", {
+        username,
+        password,
+        role: selected === 1 ? "instructor" : "participant",
+      })
+      .then((res) => res.data)
+      .then((res) => {
+        if (res.status !== "Successful") handleError(res.status);
+        else {
+          localStorage.setItem(
+            "user",
+            JSON.stringify({
+              uid: res.uid,
+              username: res.username,
+              email: res.email,
+              role: res.role,
+              join_date: res.join_date,
+            })
+          );
+          router.push("/");
+        }
+        setLoading(false);
+      })
+      .catch((err) => {
+        handleError(err);
+      });
+  };
+
   return (
-    <div className="Login m-auto pt-[25vh] flex flex-col w-[450px] h-fit p-8 gap-4">
+    <div className="login translate-x-[calc(calc(100vw-450px)/2)] translate-y-[25%] flex flex-col w-[450px] h-fit p-8 gap-4 animate-opacity">
+      <Head key={new Date().getMilliseconds()}>
+        <title> Login | LabEval </title>
+      </Head>
       <Image
         src="/labeval_logo.png"
         alt="LabEval Logo"
@@ -11,33 +66,68 @@ export default function Login() {
         height={40}
         style={{ objectFit: "contain", margin: "auto" }}
       />
-      <span className="text-xl font-medium text-center"> Login </span>
-      <form className="Login-form flex flex-col gap-6 w-full">
+      <span className="text-2xl font-medium text-center"> LOGIN </span>
+      <form
+        className="login-form flex flex-col gap-6 w-full"
+        onSubmit={handleLogin}
+      >
         <div className="form-row flex flex-col gap-1">
           <label className="text-sm font-medium"> Username </label>
           <input
-            id="username"
             type="text"
-            name="username"
             placeholder="username"
             className="h-10 px-4"
+            value={username}
+            onChange={(e) => setUserName(e.target.value)}
           />
         </div>
         <div className="form-row flex flex-col gap-1">
           <label className="text-sm font-medium"> Password </label>
           <input
-            id="password"
             type="password"
-            name="passworde"
             placeholder="password"
             className="h-10 px-4"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
           />
         </div>
-        <button className="bg-blue-600 text-slate-50 h-10 rounded-[5px]">
-          Login
+        <div className="form-row flex flex-col gap-1">
+          <label className="text-sm font-medium"> Login as </label>
+          <Select
+            options={["Participant", "Instructor"]}
+            setSelected={setSelected}
+          />
+        </div>
+        {error.length > 0 ? (
+          <span className="animate-opacity min-h-[2.5rem] rounded-[5px] w-full pl-4 pr-6 bg-red-500 text-slate-50 flex flex-row items-center justify-center relative">
+            {error}
+            <span
+              className="absolute right-4 text-xl cursor-pointer"
+              onClick={() => setError("")}
+            >
+              &times;
+            </span>
+          </span>
+        ) : null}
+        <button
+          className={
+            "text-slate-50 h-10 rounded-[5px] duration-300" +
+            (selected === 0
+              ? " bg-blue-500 hover:bg-blue-600"
+              : " bg-yellow-500 hover:bg-yellow-600")
+          }
+          onClick={handleLogin}
+        >
+          {loading ? "Logging in..." : "LOGIN"}
         </button>
         <span className="text-center">
-          {"Don't have an account?"} <Link href="/signup"> Signup </Link>{" "}
+          Already have an account?
+          <Link
+            href="/signup"
+            className={selected === 0 ? "text-blue-500" : "text-yellow-500"}
+          >
+            Signup
+          </Link>
         </span>
       </form>
     </div>
