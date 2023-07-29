86000756f88ef3e0f899ed9689178692c061fd21
Raihan-28011 <raihanrifar4@gmail.com>
2023-07-30 01:59:48 +0600

    improved navbar, added user profile popup, icons for navbar links

--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -1,4 +1,10 @@
-import { DashboardIcon, EventsIcon, LogoutIcon, SettingsIcon } from "@/icons";
+import {
+  DashboardIcon,
+  EventsIcon,
+  LogoutIcon,
+  SettingsIcon,
+  PersonIcon,
+} from "@/icons";
 import Head from "next/head";
 import Image from "next/image";
 import { useRouter } from "next/router";
@@ -86,15 +92,33 @@ function Navbar({ page }) {
             className="profile flex flex-row items-center gap-2 relative"
           >
             <span
-              className="text-slate-50 font-medium flex flex-row items-end justify-center text-xl leading-8 h-10 w-10 rounded-full bg-blue-500 cursor-pointer hover:bg-blue-600"
+              className="text-slate-50 font-medium flex flex-row items-center justify-between h-10 w-[4.5rem] px-3 rounded-[5px] bg-blue-500 cursor-pointer hover:bg-blue-600"
               onClick={() => setShowProfilePopup(true)}
             >
-              {user.username.toUpperCase()[0]}
+              <PersonIcon height="24" width="24" color="#f8fafc" />
+              <span className="border-[5px] border-solid border-b-transparent border-l-transparent border-r-transparent border-t-slate-50 w-[12px] h-[10px] translate-y-[2px]"></span>
             </span>
             {showProfilePopup ? (
-              <div className="profile-popup animate-opacity absolute right-0 top-[110%] w-60 bg-slate-50 shadow-[0_0_8px_rgba(0,0,0,0.15)] rounded-[5px] z-10 before:border-[12px] before:border-solid before:border-b-slate-50 before:border-t-transparent before:border-l-transparent before:border-r-transparent before:w-[24px] before:h-[24px] before:absolute before:right-2 before:top-[-24px] before:z-[10]">
+              <div className="profile-popup animate-opacity absolute right-0 top-[110%] min-w-[20rem] bg-slate-50 shadow-[0_0_8px_rgba(0,0,0,0.15)] rounded-[5px] z-10 before:border-[12px] before:border-solid before:border-b-slate-50 before:border-t-transparent before:border-l-transparent before:border-r-transparent before:w-[24px] before:h-[24px] before:absolute before:right-2 before:top-[-24px] before:z-[10]">
+                <div className="p-4 flex flex-row gap-3 items-center">
+                  <div className="left flex flex-row gap-4 items-center">
+                    <span className="border border-solid border-slate-900 p-1 flex flex-row items-center justify-center rounded-full">
+                      <PersonIcon height="28" width="28" />
+                    </span>
+                    <div className="name-email flex flex-col gap-1">
+                      <span>
+                        {user.username + " "}
+                        <span className="font-medium px-2 bg-red-100 border border-solud border-red-200 rounded-[5px] text-red-500">
+                          {user.role.toUpperCase()[0] + user.role.slice(1)}
+                        </span>
+                      </span>
+                      <span className="text-blue-500"> {user.email} </span>
+                    </div>
+                  </div>
+                </div>
+                <hr className="border-t border-solid border-slate-200" />
                 <div
-                  className="px-4 h-10 w-full hover:bg-slate-200 flex flex-row gap-3 items-center cursor-pointer"
+                  className="px-4 h-12 hover:bg-slate-200 flex flex-row gap-3 items-center cursor-pointer"
                   onClick={() => {
                     localStorage.removeItem("user");
                     router.push("/login");
@@ -103,7 +127,7 @@ function Navbar({ page }) {
                   <LogoutIcon width="24" height="24" />
                   <span> Logout </span>
                 </div>
-                <div className="px-4 h-10 w-full hover:bg-slate-200 flex flex-row gap-3 items-center cursor-pointer">
+                <div className="px-4 h-12 hover:bg-slate-200 flex flex-row gap-3 items-center cursor-pointer rounded-b-[5px]">
                   <SettingsIcon width="24" height="24" />
                   <span> Settings </span>
                 </div>
@@ -117,11 +141,11 @@ function Navbar({ page }) {
 }
 
 export default function Layout({ children, page }) {
+  let title = page.toUpperCase()[0] + page.slice(1) + " | LabEval";
   return (
     <div className="layout flex flex-col gap-8 px-[10%] py-8">
       <Head>
-        {" "}
-        <title> {page.toUpperCase()[0] + page.slice(1)} | LabEval </title>{" "}
+        <title>{title}</title>
       </Head>
       <Navbar page={page} />
       {children}
